c5d975818dbde6fb9f7bf174d0018c7133f7bc4e
Arthur Manceau <102350268+armanceau@users.noreply.github.com>
2024-09-19 18:42:41 +0200

    update style liste projet

--- frontend/my-app/src/components/projet/ProjetItem.vue
+++ frontend/my-app/src/components/projet/ProjetItem.vue
@@ -10,6 +10,25 @@ export default {
 </script>
 
 <template>
-    {{ nom }} | {{ _id }} {{ description }} {{ dateCreation }}
-    <a :href="`/projet/${_id}`" class="btn btn-primary mb-3">Voir le projet</a>
+    <div class="w-100 d-flex flex-column border p-3 br-1 gap-2">
+        <div class="d-flex justify-content-between rounded text-dark">
+            <div>
+                {{ nom }}
+            </div>
+            <div class="bg-secondary-custom-05 p-1 rounded">
+                {{ dateCreation }}
+            </div>
+        </div>
+        <div class="d-flex align-items-center justify-content-between">
+            <div>
+                {{ description }}
+            </div>
+            <div>
+                <a :href="`/projet/${_id}`" class="btn btn-sm btn-outline-dark d-flex align-items-center gap-2">
+                    Voir le projet
+                    <i class="bi bi-search"></i>
+                </a>
+            </div>
+        </div>
+    </div>
 </template>
--- frontend/my-app/src/views/projet/ProjetListeView.vue
+++ frontend/my-app/src/views/projet/ProjetListeView.vue
@@ -18,31 +18,32 @@ projets.value = await fetchProjets();
 		<Nav></Nav>
 	</div>
 	<div class="w-80 h-100 py-5 pr-4">
-		<div class="d-flex flex-column align-items-center w-100 h-100 border shadow-sm gap-2 br-1 w-100">
-			<div class="d-flex w-100 flex-start align-items-center justify-content-between px-4 mt-4">
+		<div class="d-flex flex-column align-items-start w-100 h-100 border shadow-sm gap-2 br-1 w-100 px-4">
+			<div class="d-flex w-100 flex-start align-items-center justify-content-between mt-4">
 				<h2>Liste des Projets</h2>
 				<a href="/add-projet" class="btn btn-outline-dark d-flex align-items-center gap-2">
 					Nouveau projet
 					<i class="bi bi-plus-circle"></i>
 				</a>
 			</div>
-		
-		
-			<ul v-if="projets.length > 0">
-				<li v-for="(projet, index) in projets" :key="index">
-					<ProjetItem
-						:nom="projet.nom"
-						:_id="projet._id"
-						:description="projet.description"
-						:dateCreation="projet.dateCreation"
-					/>		
-				</li>
-			</ul>
-			<div v-else>
-				<p>Oups ! Aucun projet disponible</p>
-				<a href="/add-projet">Créer un nouveau projet</a>
+
+			<div class="text-muted">
+				Projets actifs
 			</div>
 
+			<div v-if="projets.length > 0" v-for="(projet, index) in projets" :key="index" class="w-100">
+				<ProjetItem
+					:nom="projet.nom"
+					:_id="projet._id"
+					:description="projet.description"
+					:dateCreation="projet.dateCreation"
+				/>		
+			</div>
+
+			<div v-else>
+				<p>Oups ! Aucun projet en cours</p>
+				<a href="/add-projet">Créer un nouveau projet</a>
+			</div>
 		</div>
 	</div>
 </div>
